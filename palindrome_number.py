6da6c4a1138948eb30100ba6482f216289740e20
Andrew Siak <140351152+andrewsiak@users.noreply.github.com>
2024-06-11 18:18:25 -0400

    pushing palindrome solution start

---
+++ palindrome_number.py
@@ -0,0 +1,13 @@
+def isPalindrome(x):
+    # convert int to str 
+    # pop left and right 
+        # need to account for odd number of items in list
+    # loop through and compare first index with last index
+    # OR 
+    # keep track of index position and compare
+        # for efficiency should identify when we the middle is reached and stop
+    
+    
+    # if they are not the same return false
+    # continue loop if true 
+    # if all numbers in integer have been looped through, return true
