f207f1b54365d710df61801a3f44fb2375b8e36d
Andrew Siak <140351152+andrewsiak@users.noreply.github.com>
2024-06-12 13:39:48 -0400

    started psuedo code for longest prefix

---
+++ longest_common_prefix.py
@@ -0,0 +1,5 @@
+def longestCommonPrefix(strs):
+    # find letter at index 0 of index 0 str
+    # iterate through list of strings, and compare to index 0 of each string in list until end of list
+    # if letter matches in all stringssave to a variable "prefix"
+    # else if letter does not match, end loop
