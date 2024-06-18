312c4223d5ef55744e1efeb6f0005fb812fb12de
Andrew Siak <140351152+andrewsiak@users.noreply.github.com>
2024-06-18 14:16:27 -0400

    started code from psuedocode

--- longest_common_prefix.py
+++ longest_common_prefix.py
@@ -1,5 +1,10 @@
 def longestCommonPrefix(strs):
-    # find letter at index 0 of index 0 str
-    # iterate through list of strings, and compare to index 0 of each string in list until end of list
-    # if letter matches in all stringssave to a variable "prefix"
-    # else if letter does not match, end loop
+# find letter at index 0 of index 0 str
+    prefix = []
+    prefix = "test"
+# iterate through list of strings, and compare to index 0 of each string in list until end of list
+# if letter matches in all stringssave to a variable "prefix"
+# else if letter does not match, end loop
+    return prefix
+
+print(longestCommonPrefix(t))
--- palindrome_number.py
+++ palindrome_number.py
@@ -1,13 +1,22 @@
 def isPalindrome(x):
+    front = 0
+    back = len(x)-1
     # convert int to str 
     # pop left and right 
         # need to account for odd number of items in list
+    
+    while front < back:
+        return False
     # loop through and compare first index with last index
     # OR 
     # keep track of index position and compare
         # for efficiency should identify when we the middle is reached and stop
-    
+    front = front + 1
+    back = back - 1
     
     # if they are not the same return false
     # continue loop if true 
     # if all numbers in integer have been looped through, return true
+    if not x:
+        return True
+    
