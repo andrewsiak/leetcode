564c8facb932460b3b4f897bcf35e542d8872b14
Andrew Siak <140351152+andrewsiak@users.noreply.github.com>
2024-06-22 14:06:56 -0400

    starting code on prefix leetcode, need to work on loop

--- longest_common_prefix.py
+++ longest_common_prefix.py
@@ -1,11 +1,17 @@
 def longestCommonPrefix(strs):
 # find letter at index 0 of index 0 str
-    prefix = []
-    prefix = "test"
+    prefix_result = ""
+
+    for i, j in enumerate(strs):
+        if i[0] == j[0]:
+            prefix_result.append(i[0])
+            # this is incorrect, placeholder for now
+            return prefix_result
+
 # iterate through list of strings, and compare to index 0 of each string in list until end of list
 #   or compare first of each string, if same return true, else end loop
 # if letter matches in all stringssave to a variable "prefix"
 # else if letter does not match, end loop
-    return prefix
 
-print(longestCommonPrefix(t))
+test_case_one = ["flower", "flow", "flight"]
+print(longestCommonPrefix(test_case_one))
