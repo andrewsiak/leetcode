d5ef86c84e628f3fc2baf56b6f2d62c411ab877e
Andrew Siak <140351152+andrewsiak@users.noreply.github.com>
2024-06-25 12:16:05 -0400

    testing out parenthesis plus, minus method

--- valid_brackets.py
+++ valid_brackets.py
@@ -1,6 +1,30 @@
 def isValid(s):
-    result = 0
+    parenthesis = 0
     # create dict of key value of matching open/close brackets
+    for i in s:
+        if i == "(":
+            parenthesis += 1
+        elif i == ")":
+            parenthesis -= 1
+            if parenthesis == 0:
+                print(parenthesis)
+                return True
+            elif parenthesis != 0:
+                print(parenthesis)
+                return False
+            
     # loop through string 
+     
+
     # find matching key: value to index at end if true 
-    return result
+    
+    
+
+test_1 = "()"
+print("Case 1:", isValid(test_1))
+
+test_2 = "()[]{}"
+print("Case 2:", isValid(test_2))
+
+test_3 = "(]"
+print("Case 3:", isValid(test_3))
