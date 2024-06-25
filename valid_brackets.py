5fa9c0eaf8d21dbd733dd6d472cab0d93d7f1856
Andrew Siak <140351152+andrewsiak@users.noreply.github.com>
2024-06-25 11:52:51 -0400

    starting bracket check

---
+++ valid_brackets.py
@@ -0,0 +1,6 @@
+def isValid(s):
+    result = 0
+    # create dict of key value of matching open/close brackets
+    # loop through string 
+    # find matching key: value to index at end if true 
+    return result
