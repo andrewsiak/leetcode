ac4aa210eaf57546a30c304681009876bc5e6f72
Andrew Siak <140351152+andrewsiak@users.noreply.github.com>
2024-06-26 14:44:45 -0400

    reworking solution to use dict, incomplete

--- valid_brackets.py
+++ valid_brackets.py
@@ -1,22 +1,39 @@
 def isValid(s):
-    parenthesis = 0
     # create dict of key value of matching open/close brackets
-    for i in s:
-        if i == "(":
-            parenthesis += 1
-        elif i == ")":
-            parenthesis -= 1
-            if parenthesis == 0:
-                print(parenthesis)
-                return True
-            elif parenthesis != 0:
-                print(parenthesis)
-                return False
-            
+    result = 0
+    bracket_dict = {
+        "(": ")",
+        "{": "}",
+        "[": "]"}
+        
+        
     # loop through string 
-     
-
+    
+    for i in s:
     # find matching key: value to index at end if true 
+        if i in bracket_dict:
+            result += 1
+        
+            if result == 0:
+                print(result)
+                return True
+      
+            
+         # parenthesis = 0
+    # for i in s:
+    # # find matching key: value to index at end if true 
+    #     if i in bracket_dict:
+    #         parenthesis += 1
+    #     elif i == ")":
+    #         parenthesis -= 1
+    #         if parenthesis == 0:
+    #             print(parenthesis)
+    #             return True
+    #         elif parenthesis != 0:
+    #             print(parenthesis)
+    #             return False
+
+
     
     
 
