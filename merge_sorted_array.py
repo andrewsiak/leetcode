086cf4b02dcd812379a879318b078e4d53af0e04
Andrew Siak <140351152+andrewsiak@users.noreply.github.com>
2024-07-13 20:37:50 -0400

    working on comparing positions in each array

--- merge_sorted_array.py
+++ merge_sorted_array.py
@@ -1,18 +1,21 @@
-def merge(nums1, nums2):
+def merge(nums1, nums2, m, n):
     """
     Do not return anything, modify nums1 in-place instead.
     """
     
 
     # compare nums1 position to nums2 position 
+    while nums1.length < m + n:
+        print(nums1)
+        if nums1[i] <= nums2[j]:
+            nums1 
+            # something something
+
     # if nums1[i] is greater than nums2[i],
     # if nums1[i] is equal to nums2[i], 
     # if nums1[i] is less than nums2[i],
 
     # if it is less 
-    nums1 = nums1 + nums2
-    nums1.sort()
-    print(nums1)
 
 
 test1_nums1 = [1, 2, 3, 5]
