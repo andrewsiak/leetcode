38e4f3c45309f7e9c8c2940f299004d89beda5a2
Andrew Siak <140351152+andrewsiak@users.noreply.github.com>
2024-07-15 11:11:46 -0400

    starting plus one leetcode with initial approach thoughts in psuedo code

---
+++ plus_one.py
@@ -0,0 +1,8 @@
+class Solution:
+    def plusOne(digits):
+        # find digit at last position 
+        # if anything but 9 +1
+        # else = 0, and iterate in reverse using reversed()?
+        # and apply same rule 
+        # if 9 is found at position [0] then .insert() "1" at position [0]
+        return
