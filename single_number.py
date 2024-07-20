ff971a7f2618730eb9383dbc715350fcee8a5d8d
Andrew Siak <140351152+andrewsiak@users.noreply.github.com>
2024-07-20 10:38:02 -0400

    starting single number leetcode

---
+++ single_number.py
@@ -0,0 +1,14 @@
+def singleNumber(nums):
+    # iterate through list of nums
+    # store num as key in dict, value +1
+    # return key of value that equals 1, as all other values should be 2
+    
+    return
+        
+test_1 = [2,2,1]
+test_2 = [4,1,2,1,2]
+test_3 = [1]
+
+print("test 1", singleNumber(test_1))
+print("test 2", singleNumber(test_2))
+print("test 3", singleNumber(test_3))
